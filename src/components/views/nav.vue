f0c9b0e55929ca66ab82599279d4d8e18af48f11
Psychokiller1888 <laurentchervet@bluewin.ch>
2020-11-11 15:57:53 +0100

    :construction: added scenarios page

--- src/components/views/nav.vue
+++ src/components/views/nav.vue
@@ -3,7 +3,7 @@
 		<router-link tag="button" to="/widgets">widgets</router-link>
 		<router-link tag="button" to="/skills">skills</router-link>
 		<router-link tag="button" to="/skills">my home</router-link>
-		<router-link tag="button" to="/skills" v-if="this.$store.state.settings['scenariosActive']">scenarios</router-link>
+		<router-link tag="button" to="/scenarios" v-if="this.$store.state.settings['scenariosActive']">scenarios</router-link>
 		<router-link tag="button" to="/skills">syslog</router-link>
 		<router-link tag="button" to="/skills">alicewatch</router-link>
 		<router-link tag="button" to="/skills">admin</router-link>
---
+++ src/components/views/scenarios.vue
@@ -0,0 +1,6 @@
+<template>
+	<iframe id="nodered" title="nodered" src="http://192.168.1.168:1880"></iframe>
+</template>
+
+<style src="../css/scenarios.css" scoped></style>
+<script src="../js/scenarios.js"></script>
