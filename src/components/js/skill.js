c7e45cb43e3918eec2781d5b0efabf7fb4a6afb4
Psychokiller1888 <laurentchervet@bluewin.ch>
2020-11-13 19:18:38 +0100

    :construction: skills page

--- src/components/js/skill.js
+++ src/components/js/skill.js
@@ -1,6 +1,54 @@
+import axios from 'axios';
+
 export default {
 	name: 'skill',
+	data: function() {
+		return {
+			viewIntents: false,
+			viewInfos: false,
+			viewInstructions: false,
+			viewSkill: true
+		}
+	},
 	props: [
 		'skill'
-	]
+	],
+	methods: {
+		reloadSkill: function() {
+			axios({
+				method: 'get',
+				url: `http://${this.$store.state.settings['aliceIp']}:${this.$store.state.settings['apiPort']}/api/v1.0.1/skills/${this.skill.name}/reload`,
+				headers: {'auth': this.$store.state.loggedInUser['token'] }
+			}).then(response => {
+				if ('skill' in response.data) {
+					this.skill = response.data.skill
+				}
+			})
+		},
+		toggle: function() {
+			axios({
+				method: 'get',
+				url: `http://${this.$store.state.settings['aliceIp']}:${this.$store.state.settings['apiPort']}/api/v1.0.1/skills/${this.skill.name}/toggleActiveState`,
+				headers: {'auth': this.$store.state.loggedInUser['token'] }
+			}).then(response => (this.skill.active = !this.skill.active))
+
+		},
+		update: function() {
+			axios({
+				method: 'get',
+				url: `http://${this.$store.state.settings['aliceIp']}:${this.$store.state.settings['apiPort']}/api/v1.0.1/skills/${this.skill.name}/checkUpdate`,
+				headers: {'auth': this.$store.state.loggedInUser['token'] }
+			}).then(response => {})
+		},
+		remove: function() {
+			axios({
+				method: 'delete',
+				url: `http://${this.$store.state.settings['aliceIp']}:${this.$store.state.settings['apiPort']}/api/v1.0.1/skills/${this.skill.name}`,
+				headers: {'auth': this.$store.state.loggedInUser['token'] }
+			}).then(response => {
+				this.$destroy()
+				this.$el.parentNode.removeChild(this.$el)
+			})
+		}
+	}
 }
--- src/components/views/skill.vue
+++ src/components/views/skill.vue
@@ -2,15 +2,27 @@
 	<div class="skill vertical">
 		<div class="skillTitle">
 			<div class="skillName">{{ skill.name }}</div>
-			<div class="skillIcon"><i :class="skill.icon"></i></div>
+			<div class="skillIcon"><i :class="skill.icon" aria-hidden="true"/></div>
 		</div>
-		<div class="skillBody">
-			<div class="status">
-				<span class="fa-stack" v-if="skill.active">
-					<i class="fas fa-circle fa-stack-2x black"></i>
-					<i class="fas fa-circle fa-stack-1x green"></i>
-				</span>
+		<div class="skillBody" v-if="viewInfos">
+
+		</div>
+		<div class="skillBody" v-if="viewInstructions">
+
+		</div>
+		<div class="skillBody" v-if="viewIntents"></div>
+		<div class="skillBody" v-if="viewSkill">
+			<div class="clickable" @click="viewSkill = viewIntents = viewInfos = false; viewInstructions = true">Instructions</div>
+			<div class="clickable" @click="viewSkill = viewInstructions = viewInfos = false; viewIntents = true">Intents</div>
+			<div class="clickable" @click="viewSkill = viewIntents = viewInstructions = false; viewInfos = true">Infos</div>
+			<div class="skillActions">
+				<i class="fas fa-cog clickable" aria-hidden="true" v-if="Object.keys(skill.settings).length"/>
+				<i class="fas fa-power-off clickable" aria-hidden="true" v-if="!skill.required" @click="toggle"/>
+				<i class="fas fa-sync clickable" aria-hidden="true" v-if="skill.active" @click="reloadSkill"/>
+				<i class="fas fa-arrow-alt-circle-up clickable" aria-hidden="true" v-if="skill.updateAvailable" @click="update"/>
+				<i class="far fa-times-circle clickable" aria-hidden="true" v-if="!skill.required" @click="remove"/>
 			</div>
+			<div class="skillStatus" :class="skill.active ? 'active' : 'disabled'"/>
 		</div>
 	</div>
 </template>
