04adaacbf196d49846b528cf09a7762e77b5231a
Psychokiller1888 <laurentchervet@bluewin.ch>
2020-11-13 15:48:33 +0100

    :construction: tired

--- src/components/views/skill.vue
+++ src/components/views/skill.vue
@@ -4,6 +4,14 @@
 			<div class="skillName">{{ skill.name }}</div>
 			<div class="skillIcon"><i :class="skill.icon"></i></div>
 		</div>
+		<div class="skillBody">
+			<div class="status">
+				<span class="fa-stack" v-if="skill.active">
+					<i class="fas fa-circle fa-stack-2x black"></i>
+					<i class="fas fa-circle fa-stack-1x green"></i>
+				</span>
+			</div>
+		</div>
 	</div>
 </template>
 
