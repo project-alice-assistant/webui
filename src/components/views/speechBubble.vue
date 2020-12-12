2fc1ae354f383cdc811225e18941d1337c3e38a8
Psychokiller1888 <laurentchervet@bluewin.ch>
2020-12-12 16:29:46 +0100

    Cleanup ecma6

--- src/components/views/speechBubble.vue
+++ src/components/views/speechBubble.vue
@@ -1,20 +1,29 @@
 <template>
 <div>
-	<div v-if="msg.topic == 'hermes/hotword/#/detected'" class="userSpeech male extend"> Hey Alice! </div>
-	<div v-if="msg.topic == 'hermes/tts/say'" class="aliceSpeech extend"> {{ JSON.parse(msg.payloadString)['text'] }}</div>
-	<div v-if="msg.topic == 'hermes/nlu/query'" class="userSpeech male extend"> {{ JSON.parse(msg.payloadString)['input'] }}</div>
-	<div v-if="msg.topic == 'hermes/dialogueManager/sessionEnded'" class="sessionEnded extend">---- Session ended: {{ JSON.parse(msg.payloadString)['termination']['reason'] }} ----</div>
-	<div v-if="msg.topic == 'hermes/asr/partialTextCaptured'" class="userSpeech male extend"> {{ JSON.parse(msg.payloadString)['text'] }} ... </div>
+	<div v-if="msg.topic === 'hermes/hotword/#/detected'" class="userSpeech male extend"> Hey Alice!</div>
+	<div v-if="msg.topic === 'hermes/tts/say'" class="aliceSpeech extend"> {{
+			JSON.parse(msg.payloadString)['text']
+		}}
+	</div>
+	<div v-if="msg.topic === 'hermes/nlu/query'" class="userSpeech male extend"> {{
+			JSON.parse(msg.payloadString)['input']
+		}}
+	</div>
+	<div v-if="msg.topic === 'hermes/dialogueManager/sessionEnded'" class="sessionEnded extend">---- Session ended:
+		{{ JSON.parse(msg.payloadString)['termination']['reason'] }} ----
+	</div>
+	<div v-if="msg.topic === 'hermes/asr/partialTextCaptured'" class="userSpeech male extend">
+		{{ JSON.parse(msg.payloadString)['text'] }} ...
+	</div>
 
 </div>
 </template>
 
 <script>
 export default {
-	name: "speechBubble",
+	name: 'speechBubble',
 	data: function () {
-		return {
-		}
+		return {}
 	},
 	props: [
 		'msg'
