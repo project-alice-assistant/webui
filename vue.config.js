7cf6704b411d327ca13f76f99581ef1dc9bc3c14
Psychokiller1888 <laurentchervet@bluewin.ch>
2021-01-29 19:34:41 +0100

    fix build path for GH Actions

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-	outputDir: path.resolve(__dirname, '../ProjectAlice/core/webui/public')
+	outputDir: path.resolve(__dirname, 'dist')
 }
 
